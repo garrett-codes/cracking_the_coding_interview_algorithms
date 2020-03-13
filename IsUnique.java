596283c73b9a0724a8bcc9b4be5b24dfb119dd45
Garrett Halstein <garrett.halstein@emory.edu>
2020-03-12 20:52:50 -0400

    added some comments

--- IsUnique.java
+++ IsUnique.java
@@ -1,23 +1,26 @@
 import java.util.Set;
 import java.util.HashSet;
 
-public class IsUnique {
 
-  public static boolean isUnique(String str) {
-    
-    Set<Character> set = new HashSet<>();
-    
-    for (int i = 0; i < str.length(); ++i) {
-      if (set.contains(str.charAt(i))) {
-        return false;
+public class IsUnique { // the class or object. In this case the entire program
+
+  // this is a method that returns a boolean
+  public static boolean isUnique(String str) { /* the public part means you can access 
+                                               this method outside of the object    */
+    Set<Character> set = new HashSet<>();     /* static this method is used by the class
+                                               and not the instance of the object*/
+    for (int i = 0; i < str.length(); ++i) { // typical for loop starting at 0
+      if (set.contains(str.charAt(i))) { // contains finds if the set contains the char of the string at the ith index
+        return false; // if it does then it returns false cause theres duplicates
       } else {
-        set.add(str.charAt(i));
+        set.add(str.charAt(i)); // otherwise it adds it to the set to keep track of it
       }
     }
-
+    // if it gets to the end then it must be unique
     return true;
   }
-  public static void main(String[] args) {
-    System.out.println(IsUnique.isUnique("hello"));
+  public static void main(String[] args) { // this is the main method for the class that gets executed when you compile and run the program as an executable
+    System.out.println(IsUnique.isUnique("hello")); // prints out false
+    System.out.println("unique"); // prints out true
   }
 }
